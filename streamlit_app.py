9a52d9426553bdc69c103a85b33fea58f7a55da1
morispolanco <morispolanco@gmail.com>
2023-12-31 02:50:38 -0600

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import requests
+
+def make_api_call(question, api_key):
+    url = "https://api.perplexity.ai/chat/completions"
+
+    payload = {
+        "model": "mistral-7b-instruct",
+        "messages": [
+            {
+                "role": "system",
+                "content": "Be precise and concise."
+            },
+            {
+                "role": "user",
+                "content": question
+            }
+        ]
+    }
+
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "Authorization": f"Bearer {api_key}"  # Incluye la API key en los encabezados
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+    return response.json()["choices"][0]["message"]["content"]
+
+def main():
+    st.title("Preguntas sobre las leyes de Guatemala")
+
+    question = st.text_input("Haz tu pregunta:")
+    api_key = "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"  # Reemplaza con tu clave real
+
+    if st.button("Obtener respuesta"):
+        if question:
+            answer = make_api_call(question, api_key)
+            st.write("Respuesta:", answer)
+        else:
+            st.warning("Por favor, ingresa una pregunta.")
+
+if __name__ == "__main__":
+    main()
